5d05ce8b398e0c307d588948abca2ed008843df7
romansarker78 <54774634+romansarker78@users.noreply.github.com>
2020-03-29 13:19:48 +0600

    Add files via upload

---
+++ index.js
@@ -0,0 +1,112 @@
+document.querySelector("#alif").addEventListener("click", function () {
+    var audio = new Audio("audio/alif.mp3");
+    audio.play();
+});
+document.querySelector("#ba").addEventListener("click", function () {
+    var audio = new Audio("audio/ba.mp3");
+    audio.play();
+});
+document.querySelector("#ta").addEventListener("click", function () {
+    var audio = new Audio("audio/ta.mp3");
+    audio.play();
+});
+document.querySelector("#tha").addEventListener("click", function () {
+    var audio = new Audio("audio/th.mp3");
+    audio.play();
+});
+document.querySelector("#jim").addEventListener("click", function () {
+    var audio = new Audio("audio/jim.mp3");
+    audio.play();
+});
+document.querySelector("#ha").addEventListener("click", function () {
+    var audio = new Audio("audio/ha.mp3");
+    audio.play();
+});
+document.querySelector("#kha").addEventListener("click", function () {
+    var audio = new Audio("audio/kha.mp3");
+    audio.play();
+});
+document.querySelector("#dal").addEventListener("click", function () {
+    var audio = new Audio("audio/dal.mp3");
+    audio.play();
+});
+document.querySelector("#ra").addEventListener("click", function () {
+    var audio = new Audio("audio/ra.mp3");
+    audio.play();
+});
+document.querySelector("#ja").addEventListener("click", function () {
+    var audio = new Audio("audio/ja.mp3");
+    audio.play();
+});
+document.querySelector("#sin").addEventListener("click", function () {
+    var audio = new Audio("audio/sin.mp3");
+    audio.play();
+});
+document.querySelector("#shin").addEventListener("click", function () {
+    var audio = new Audio("audio/shin.mp3");
+    audio.play();
+});
+document.querySelector("#sad").addEventListener("click", function () {
+    var audio = new Audio("audio/sad.mp3");
+    audio.play();
+});
+document.querySelector("#dad").addEventListener("click", function () {
+    var audio = new Audio("audio/dad.mp3");
+    audio.play();
+});
+document.querySelector("#toa").addEventListener("click", function () {
+    var audio = new Audio("audio/toa.mp3");
+    audio.play();
+});
+document.querySelector("#joa").addEventListener("click", function () {
+    var audio = new Audio("audio/joa.mp3");
+    audio.play();
+});
+document.querySelector("#ain").addEventListener("click", function () {
+    var audio = new Audio("audio/ain.mp3");
+    audio.play();
+});
+document.querySelector("#gh").addEventListener("click", function () {
+    var audio = new Audio("audio/gh.mp3");
+    audio.play();
+});
+document.querySelector("#fa").addEventListener("click", function () {
+    var audio = new Audio("audio/fa.mp3");
+    audio.play();
+});
+document.querySelector("#kof").addEventListener("click", function () {
+    var audio = new Audio("audio/kof.mp3");
+    audio.play();
+});
+document.querySelector("#kaf").addEventListener("click", function () {
+    var audio = new Audio("audio/kaf.mp3");
+    audio.play();
+});
+document.querySelector("#lam").addEventListener("click", function () {
+    var audio = new Audio("audio/lam.mp3");
+    audio.play();
+});
+document.querySelector("#mim").addEventListener("click", function () {
+    var audio = new Audio("audio/mim.mp3");
+    audio.play();
+});
+document.querySelector("#nun").addEventListener("click", function () {
+    var audio = new Audio("audio/nun.mp3");
+    audio.play();
+});
+document.querySelector("#waw").addEventListener("click", function () {
+    var audio = new Audio("audio/waw.mp3");
+    audio.play();
+});
+document.querySelector("#haa").addEventListener("click", function () {
+    var audio = new Audio("audio/haa.mp3");
+    audio.play();
+});
+/*document.querySelector("#hamza").addEventListener("click", function () {
+    var audio = new Audio("audio/alif.mp3");
+    audio.play();
+});*/
+document.querySelector("#yaa").addEventListener("click", function () {
+    var audio = new Audio("audio/yaa.mp3");
+    audio.play();
+});
